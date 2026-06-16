8c3c630556a4fd7eb7d5d99d06db09fcbbe39103
Rocco <roccominetola24@gmail.com>
2026-06-16 14:55:46 -0400

    Step #9 (I was stuck on this one for a while getting "Access denied" and I kept changing things but after a while I tried logging out of admin and then back into admin then it worked)

--- MinetolaMidtermA/Controllers/TeamsController.cs
+++ MinetolaMidtermA/Controllers/TeamsController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinetolaMidtermA.Data;
+using MinetolaMidtermA.Models;
 
 namespace MinetolaMidtermA.Controllers
 {
@@ -19,5 +21,25 @@ namespace MinetolaMidtermA.Controllers
             return View(teams);
         }
 
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(team);
+            }
+
+            _context.Team.Add(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- MinetolaMidtermA/Program.cs
+++ MinetolaMidtermA/Program.cs
@@ -4,19 +4,19 @@ using MinetolaMidtermA.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,13 +24,13 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -42,5 +42,29 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    var adminRole = "Admin";
+    if (!roleManager.RoleExistsAsync(adminRole).GetAwaiter().GetResult())
+    {
+        roleManager.CreateAsync(new IdentityRole(adminRole)).GetAwaiter().GetResult();
+    }
+
+    var adminEmail = "examadmin@gc.ca";
+    var admin = userManager.FindByEmailAsync(adminEmail).GetAwaiter().GetResult();
+    if (admin == null)
+    {
+        admin = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+        var createResult = userManager.CreateAsync(admin, "Test123$").GetAwaiter().GetResult();
+        if (createResult.Succeeded)
+        {
+            userManager.AddToRoleAsync(admin, adminRole).GetAwaiter().GetResult();
+        }
+    }
+}
 
 app.Run();
