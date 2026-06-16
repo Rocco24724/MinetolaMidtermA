c8a5b97ce07e12f1b2bd30a6e9a19724c3239d90
Rocco <roccominetola24@gmail.com>
2026-06-16 13:27:13 -0400

    Created Team model with the following properties

---
+++ MinetolaMidtermA/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿namespace MinetolaMidtermA.Models
+{
+    public class Team
+    {
+        public int Teamid { get; set; }
+
+        public string Country { get; set; }
+
+        public string Group { get; set; }
+
+        public int Ranking { get; set; }
+
+        public DateTime FirstMatch { get; set; }
+
+    }
+}
