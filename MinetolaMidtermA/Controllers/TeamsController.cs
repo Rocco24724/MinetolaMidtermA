f734570b259708154dd36152fd1d81e5087021f0
Rocco <roccominetola24@gmail.com>
2026-06-16 14:15:26 -0400

    Added to the TeamsController so I am able to view step #7

--- MinetolaMidtermA/Controllers/TeamsController.cs
+++ MinetolaMidtermA/Controllers/TeamsController.cs
@@ -1,8 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinetolaMidtermA.Data;
 
 namespace MinetolaMidtermA.Controllers
 {
     public class TeamsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var teams = await _context.Team.ToListAsync();
+            return View(teams);
+        }
     }
 }
